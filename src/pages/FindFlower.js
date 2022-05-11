bcda1cab34d8763644968cfd448abd9822ad80e9
ivan <ivan_kozyrev_02@mail.ru>
2022-05-11 11:15:36 +0300

    add find flower

--- src/pages/FindFlower.js
+++ src/pages/FindFlower.js
@@ -1,18 +1,52 @@
 import "../pages/styles/find_flower.css";
-import {Link} from "react-router-dom";
+import axios from "axios";
+import React, {useState} from "react";
+import img from "../assets/draft.jpg";
+
+const Product = ({item}) => {
+    console.log(item["images"][0])
+    return (
+        <div className="container_image">
+            <img className="icon1" src={`data:image/jpeg;base64,${item["images"][0]}`} alt="img"/>
+            <div className="tag">{item["name"]}</div>
+            <div className="tag">{item["prob"]}%</div>
+        </div>
+    );
+};
 
 const FindFlower = () => {
+    const [file, setFile] = useState('')
+    const [data, setData] = useState('')
+
+    const findFlowerSubmit = async (e) => {
+        const data = new FormData();
+        data.append('img', file);
+        e.preventDefault();
+        axios.post(
+            'http://localhost:8880/find_flower', data
+        ).then(response => {
+                if (response.status === 200) {
+                    setData(JSON.parse(response['data']))
+                }
+            }
+        )
+    }
+
     return (
         <div className="find-container">
             <div className="container-wrapper">
                 <h1 className="title">Find a flower by photo</h1>
-                <form className="form">
+                <form className="form" onSubmit={findFlowerSubmit}>
                     <div className="file btn btn-lg btn-primary">
-                        Add Photo<input type="file" name="file" accept=".jpg,.jpeg,.png"/>
+                        Add Photo<input type="file" name="file" accept=".jpg,.jpeg,.png,.webp"
+                                        onChange={e => setFile(e.target.files[0])}/>
                     </div>
-                    <Link to="/find_flower">
-                        <button className="form-button">FIND</button>
-                    </Link>
+                    <div className="poojaD">
+                        {data && data.map((item) => (
+                            <Product item={item} key={item.name}/>
+                        ))}
+                    </div>
+                    <button className='form-button' type="submit">FIND</button>
                 </form>
             </div>
         </div>
